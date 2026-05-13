220269861a4bca42e49f5395791e32ddb3e9d3cb
Mansur550 <mansurislam59@gmail.com>
2026-05-14 02:14:38 +0600

    1St component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@
 import './App.css'
 
 function App() {
- 
+
   return (
     <>
       <h1>RRACT + VITE</h1>
-
+      <Person />
 
     </>
+  )
+}
+
+function Person() {
+  return (
+
+    <p>
+      I am a person.
+    </p>
   )
 }
 
