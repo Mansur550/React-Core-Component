83ce554b2b950cc16f50182376ba3531922b937a
Mansur550 <mansurislam59@gmail.com>
2026-05-16 00:54:37 +0600

    Passing data in component

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,34 @@ function App() {
   return (
     <>
       <h1 className='text-xl'>REACT Core Concepts</h1>
+      <Student name="Mansur" dept="CSE " tool="Mobile"></Student>
+      <Student name="Dristy" dept="CSE" tool="Laptop"></Student>
+      <Student name="Tahsin" dept="CSE" tool="Pc"></Student>
+
+
 
 
     </>
   )
 }
+
+function Student(props) {
+  console.log(props)
+  return (
+    <div style={{
+      border: '2px solid green',
+      borderRadius: '10px',
+      padding: '10px',
+      margin: '20px'
+    }}
+    >
+      <p>Name:{props.name}</p>
+      <p>Dept:{props.dept}</p>
+      <p>Device:{props.tool}</p>
+    </div>
+  )
+}
+
 
 function Person() {
   const age = 30
