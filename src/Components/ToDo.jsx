ba4726ff2f1f806a15f14e50cf8acc8c1ebdb690
Mansur550 <mansurislam59@gmail.com>
2026-05-16 02:02:37 +0600

    Conditionl Rendering with ternaryOperator' ?'

--- src/Components/ToDo.jsx
+++ src/Components/ToDo.jsx
@@ -5,13 +5,20 @@
 //     )
 // }
 
+// export default function ToDo({ task, isDone }) {
+
+//     if (isDone === true) {
+//         return <li>Done: {task}</li>
+
+//     } else {
+//         return < li>Pending: {task}</li>
+
+//     }
+// }
+
+
+//Ternary operator
+
 export default function ToDo({ task, isDone }) {
-
-    if (isDone === true) {
-        return <li>Done: {task}</li>
-
-    } else {
-        return < li>Pending: {task}</li>
-
-    }
+    return isDone ? <li>Done:{task}</li> : <li>Pending:{task}</li>
 }
